d9266d2180ef425dabc72e370a0fbeed669b2dac
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-15 12:44:24 +0300

    Config static and media url

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls.static import static
 from django.urls import path, re_path
 
 from shop import settings
@@ -9,6 +8,3 @@ urlpatterns = [
     re_path(r'^(?P<category_slug>[-\w]+)/$', views.product_list, name='product_list_by_category'),
     re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/settings.py
+++ shop/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = []
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
