76edf6a7e5f1a6fadfc505be29d71696f6912837
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-15 10:08:00 +0300

    Define get_absolute_url and a class attribute 'objects'

--- myshop/models.py
+++ myshop/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.CharField(max_length=255, db_index=True, unique=True)
+    objects = models.Manager()
 
     class Meta:
         ordering = ('name',)
@@ -12,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -25,6 +30,7 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
 
     class Meta:
         ordering = ('name',)
@@ -32,3 +38,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.id, self.slug])
