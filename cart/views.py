23acf76e18d223dcc3d12fc23e385225f4d832f8
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-11-27 13:38:07 +0300

    Fix: fix form field name

--- cart/views.py
+++ cart/views.py
@@ -30,5 +30,5 @@ def cart_detail(request):
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={
             'quantity': item['quantity'],
-            'override': True})
+            'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
