3c6ab1a1026ac65694385db43243971870f941a1
Kiryl Dumanski <Kocherizhkin@gmail.com>
2022-12-26 14:59:30 +0300

    Feat: Set up permission classes

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from api.permissions import IsAdminOrReadOnly
 from api.serializers import ProductSerializer, CategorySerializer
@@ -8,13 +9,13 @@ from myshop.models import Product, Category
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = (IsAdminOrReadOnly, )
+    permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
 
 
 # class ProductApiView(generics.ListAPIView):
